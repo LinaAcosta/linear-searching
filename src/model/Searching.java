fbc92c9f23f0551ddd10c2c3253c44e56bb67924
Juan Reyes <seyerman@gmail.com>
2019-02-25 13:01:53 -0500

    Linear searching with threads Java projecte created

---
+++ src/model/Searching.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Searching {
+	private long[] numbers;
+	
+	public Searching(long[] nums) {
+		numbers = nums;
+	}
+	
+	public int countNumbers(long num, int start, int end) {
+		
+	}
+}
