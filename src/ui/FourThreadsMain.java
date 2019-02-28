f4ca2e1b47af858347c7afb519b03acd344cf1bd
Juan Reyes <seyerman@gmail.com>
2019-02-28 01:12:18 -0500

    Created tow additional mains for two and four threads. Are interesting the results in execution time.

--- src/ui/FourThreadsMain.java
+++ src/ui/FourThreadsMain.java
@@ -6,34 +6,22 @@ import java.util.List;
 import model.Searching;
 import thread.SearchingThread;
 
-public class TenThreadsMain {
+public class FourThreadsMain {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Program started.");
 		System.out.println("Generating random integer list...");
 		List<Integer> intList = generateRandomList(100000, 1000);
 		System.out.println("Random integer list generated.");
 		
-		Searching sch1 = new Searching(intList, 5, 0, 10000);
-		Searching sch2 = new Searching(intList, 5, 10000, 20000);
-		Searching sch3 = new Searching(intList, 5, 20000, 30000);
-		Searching sch4 = new Searching(intList, 5, 30000, 40000);
-		Searching sch5 = new Searching(intList, 5, 40000, 50000);
-		Searching sch6 = new Searching(intList, 5, 50000, 60000);
-		Searching sch7 = new Searching(intList, 5, 60000, 70000);
-		Searching sch8 = new Searching(intList, 5, 70000, 80000);
-		Searching sch9 = new Searching(intList, 5, 80000, 90000);
-		Searching sch10 = new Searching(intList, 5, 90000, 100000);
+		Searching sch1 = new Searching(intList, 5, 0, 25000);
+		Searching sch2 = new Searching(intList, 5, 25000, 50000);
+		Searching sch3 = new Searching(intList, 5, 50000, 75000);
+		Searching sch4 = new Searching(intList, 5, 75000, 100000);
 		
 		SearchingThread st1 = new SearchingThread(sch1);
 		SearchingThread st2 = new SearchingThread(sch2);
 		SearchingThread st3 = new SearchingThread(sch3);
 		SearchingThread st4 = new SearchingThread(sch4);
-		SearchingThread st5 = new SearchingThread(sch5);
-		SearchingThread st6 = new SearchingThread(sch6);
-		SearchingThread st7 = new SearchingThread(sch7);
-		SearchingThread st8 = new SearchingThread(sch8);
-		SearchingThread st9 = new SearchingThread(sch9);
-		SearchingThread st10 = new SearchingThread(sch10);
 		
 		System.out.println("Counting numbers...");
 		
@@ -43,23 +31,11 @@ public class TenThreadsMain {
 		st2.start();
 		st3.start();
 		st4.start();
-		st5.start();
-		st6.start();
-		st7.start();
-		st8.start();
-		st9.start();
-		st10.start();
 		
 		st1.join();
 		st2.join();
 		st3.join();
 		st4.join();
-		st5.join();
-		st6.join();
-		st7.join();
-		st8.join();
-		st9.join();
-		st10.join();
 		
 		long endTime = System.currentTimeMillis();
 				
@@ -67,16 +43,10 @@ public class TenThreadsMain {
 		output += sch2.getTotal();
 		output += sch3.getTotal();
 		output += sch4.getTotal();
-		output += sch5.getTotal();
-		output += sch6.getTotal();
-		output += sch7.getTotal();
-		output += sch8.getTotal();
-		output += sch9.getTotal();
-		output += sch10.getTotal();
 		
 		System.out.println("Numbers counted.");
 		System.out.println("5 is "+output+" times in the random array");
-		System.out.println("Duration with 10 threads: "+(endTime-initTime));
+		System.out.println("Duration with 4 threads: "+(endTime-initTime));
 	}
 	
 	public static List<Integer> generateRandomList(int size, int max) {
--- src/ui/TwoThreadsMain.java
+++ src/ui/TwoThreadsMain.java
@@ -4,28 +4,39 @@ import java.util.LinkedList;
 import java.util.List;
 
 import model.Searching;
+import thread.SearchingThread;
 
-public class WithoutExtraThreadsMain {
-	public static void main(String[] args) {
+public class TwoThreadsMain {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Program started.");
 		System.out.println("Generating random integer list...");
 		List<Integer> intList = generateRandomList(100000, 1000);
 		System.out.println("Random integer list generated.");
 		
+		Searching sch1 = new Searching(intList, 5, 0, 50000);
+		Searching sch2 = new Searching(intList, 5, 50000, 100000);
+		
+		SearchingThread st1 = new SearchingThread(sch1);
+		SearchingThread st2 = new SearchingThread(sch2);
+		
 		System.out.println("Counting numbers...");
 		
 		long initTime = System.currentTimeMillis();
 		
-		Searching sch = new Searching(intList, 5, 0, intList.size());
+		st1.start();
+		st2.start();
 		
-		sch.countNumbers();
-		
+		st1.join();
+		st2.join();
+	
 		long endTime = System.currentTimeMillis();
+				
+		int output  = sch1.getTotal();
+		output += sch2.getTotal();
 		
 		System.out.println("Numbers counted.");
-		int output = sch.getTotal();
 		System.out.println("5 is "+output+" times in the random array");
-		System.out.println("Duration with 1 thread: "+(endTime-initTime));
+		System.out.println("Duration with 2 threads: "+(endTime-initTime));
 	}
 	
 	public static List<Integer> generateRandomList(int size, int max) {
